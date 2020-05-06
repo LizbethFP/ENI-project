93053025794652786fe8b002940efe18ce20f09a
Liz Felix <lizbeth.fp@gmail.com>
2020-05-06 20:14:19 +1000

    feat(app-module):sets up routing module for product list and about us component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'about-us', component: AboutUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  ProductListComponent,
+  AboutUsComponent,
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CarouselModalComponent } from './components/carousel-modal/carousel-modal.component';
+import { CardGroupsComponent } from './components/card-groups/card-groups.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CarouselModalComponent,
+    CardGroupsComponent,
+    routingComponents,
   ],
   imports: [
     BrowserModule,
