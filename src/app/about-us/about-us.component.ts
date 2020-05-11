129afd384a13f6524a5b9130a8ef47b890189796
Liz Felix <lizbeth.fp@gmail.com>
2020-05-11 21:17:30 +1000

    feat(about-us):creates about us section

---
+++ src/app/about-us/about-us.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.scss']
+})
+export class AboutUsComponent implements OnInit {
+
+  mission = './../../assets/target.png';
+  vision = './../../assets/vision.png';
+  values = './../../assets/value.png';
+  email = './../../assets/email.png';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
